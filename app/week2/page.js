10cf2d27dac8ce88f8ab7e302250e2ce43263e3b
Hamdy Ibrahim <ha_madien@yahoo.com>
2026-09-09 09:48:34 -0600

    Week 2 completed

---
+++ app/week2/page.js
@@ -0,0 +1,10 @@
+import Title from "./title";
+export default function Page() {
+  return (
+    <main>
+      <h1 className="text-3xl font-bold text-blue-600">Week 2 Route/Page</h1>
+      <p>More content</p>
+      <Title />
+    </main>
+  );
+}
