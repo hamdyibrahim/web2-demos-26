eca64d1f7c2c02615ddf09fd539e7a5f0606c16b
Hamdy Ibrahim <ha_madien@yahoo.com>
2026-09-11 15:12:03 -0600

    Demo JSX

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ export default function Page() {
     <main>
       <h1 className="text-3xl font-bold text-blue-600">Web 2 Demos</h1>
       <p>Click one of the following links</p>
-      <Link href="week2" className="text-underline text-blue-400">
+      <Link href="week2" className="text-underline text-blue-700">
         Go to Week 2
       </Link>
     </main>
